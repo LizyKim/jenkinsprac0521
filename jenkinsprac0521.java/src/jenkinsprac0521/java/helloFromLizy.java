3cc2453c81833c7b086d50e8e6ca7c5617be29bd
lizyrychee <hiiamlizy@g.hongik.ac.kr>
2025-05-22 13:23:16 +0900

    fifth commit, why isn't it working properly?

--- jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
+++ jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
@@ -2,6 +2,6 @@ package jenkinsprac0521.java;
 
 public class helloFromLizy {
 	public static void main(String[] args) {
-        System.out.println("Greetings from lizy, 13:14 22 May.");
+        System.out.println("Greetings from lizy, 13:22 22 May");
     }
 }
