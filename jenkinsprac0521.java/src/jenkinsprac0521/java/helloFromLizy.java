17738f8543a1d63475941a9f640c7297db5f831e
lizyrychee <hiiamlizy@g.hongik.ac.kr>
2025-05-22 10:05:41 +0900

    fifth commit to test if automatic build is running. 10:00 22 may.

--- jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
+++ jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
@@ -2,6 +2,6 @@ package jenkinsprac0521.java;
 
 public class helloFromLizy {
 	public static void main(String[] args) {
-        System.out.println("Greetings from lizy");
+        System.out.println("Greetings from lizy, 10:05 22 May");
     }
 }
