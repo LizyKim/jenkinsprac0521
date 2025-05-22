f8e022e352ce115a0246b280f02a71f076a1714e
lizyrychee <hiiamlizy@g.hongik.ac.kr>
2025-05-22 14:16:39 +0900

    sixth commit, found out some configurations needed for automatic email. modified email settings including : SMTP, port, user email(g), password(from g- app password), 14:16.

--- jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
+++ jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
@@ -2,6 +2,6 @@ package jenkinsprac0521.java;
 
 public class helloFromLizy {
 	public static void main(String[] args) {
-        System.out.println("Greetings from lizy, 13:50 22 May");
+        System.out.println("Greetings from lizy, 14:15 22 May");
     }
 }
