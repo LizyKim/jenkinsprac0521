e79a09ca06b433e0fed524a67cc649b203d00bab
lizyrychee <hiiamlizy@g.hongik.ac.kr>
2025-05-26 16:11:37 +0900

    first commit from branchTwo. in order to prevent error(expected to be syntax).

--- jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
+++ jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
@@ -2,6 +2,6 @@ package jenkinsprac0521.java;
 
 public class helloFromLizy {
 	public static void main(String[] args) {
-        System.out.println("Greetings from lizy, 16:07 26 May");
+        System.out.println("Greetings from lizy, 16:11 26 May");
     }
 }
