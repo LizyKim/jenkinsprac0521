78f47462c3b1e2f1579632bff43476d02a1ccae0
lizyrychee <hiiamlizy@g.hongik.ac.kr>
2025-05-26 16:26:38 +0900

    eighth-2 commit, after modifying jenkinsfile(syntax error)

--- jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
+++ jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
@@ -2,6 +2,6 @@ package jenkinsprac0521.java;
 
 public class helloFromLizy {
 	public static void main(String[] args) {
-        System.out.println("Greetings from lizy, 16:21 26 May");
+        System.out.println("Greetings from lizy, 16:26 26 May");
     }
 }
