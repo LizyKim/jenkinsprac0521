62e8254bc7d073e2e3f34cbf4d9b7ab84c443878
lizyrychee <hiiamlizy@g.hongik.ac.kr>
2025-05-26 16:21:06 +0900

    eighth commit from branchTwo. unnecessary files cleaned up, branch setting from Jenkins modified.

--- jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
+++ jenkinsprac0521.java/src/jenkinsprac0521/java/helloFromLizy.java
@@ -2,6 +2,6 @@ package jenkinsprac0521.java;
 
 public class helloFromLizy {
 	public static void main(String[] args) {
-        System.out.println("Greetings from lizy, 16:11 26 May");
+        System.out.println("Greetings from lizy, 16:19 26 May");
     }
 }
